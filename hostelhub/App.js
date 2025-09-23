fd509f20fa8a9de959bebb64501ede8353742a18
kritgarg <kritg0160@gmail.com>
2025-09-23 12:00:00 +0530

    adding tab navigators

--- hostelhub/App.js
+++ hostelhub/App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from './screens/Home';
+import LostandFound from './screens/Landf';
+import Qraccess from './screens/Qr';
+import Profile from './screens/Profile';
+import Services from './screens/Services';
+import Marketplace from './screens/Marketplace';
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Home" component={Home} />
+            <Tab.Screen name="Lost and Found" component={LostandFound} />
+            <Tab.Screen name="Marketplace" component={Marketplace} />
+            <Tab.Screen name="Profile" component={Profile} />
+            <Tab.Screen name="Services" component={Services} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ hostelhub/screens/Marketplace.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Marketplace() {
+  return (
+    <View>
+      <Text>Marketplace</Text>
+    </View>
+  );
+}
